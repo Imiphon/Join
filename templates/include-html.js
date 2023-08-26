eb6955973ea3c695cafe4a007890736ec4f13170
Vitalie Tanasiev <vitalie.tanasiev@dachpc.com>
2023-08-26 15:22:33 +0200

    finished footer click  logic

--- templates/include-html.js
+++ templates/include-html.js
@@ -10,6 +10,7 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+    setDefaultContentToSummary();
 }
 
 
@@ -34,53 +35,37 @@ function showSubmenuMobile() {
 
 
 // ********************************************************************************
-// ********* This logic changes footer menu icons when clicked ********************
+// This logic changes footerMenu icons when clicked & sets summary as default content
 // ********************************************************************************
-let summaryBtnNotClicked = false;
-let addTaskBtnNotClicked = true;
-let contactsBtnNotClicked = true;
-let boardBtnNotClicked = true;
+let buttonStates = {
+    summary: false,
+    add: false,
+    contacts: false,
+    board: false
+};
 
-
-function showSummary() {
-    let imgSummaryFooterMenu = document.getElementById("imgSummaryFooterMenu");
-    if (summaryBtnNotClicked) {
-        imgSummaryFooterMenu.src = "../assets/img/summary_button_footer_mobile.png";
-    } else {
-        imgSummaryFooterMenu.src = "../assets/img/summary_icon_default_mobile.png";
+function toggleButton(buttonName) {
+    for (let btn in buttonStates) {
+        if (btn !== buttonName) {
+            let img = document.getElementById(`img${btn.charAt(0).toUpperCase() + btn.slice(1)}FooterMenu`);
+            img.src = `../assets/img/${btn}_button_default.png`;
+            buttonStates[btn] = false;
+        }
     }
-    summaryBtnNotClicked = !summaryBtnNotClicked;
+
+    let imgFooterMenu = document.getElementById(`img${buttonName.charAt(0).toUpperCase() + buttonName.slice(1)}FooterMenu`);
+    
+    if (!buttonStates[buttonName]) {
+        imgFooterMenu.src = `../assets/img/${buttonName}_button_clicked.png`;
+    } else {
+        imgFooterMenu.src = `../assets/img/${buttonName}_button_default.png`;
+    }
+    
+    buttonStates[buttonName] = !buttonStates[buttonName];
 }
 
-function showAddTask() {
-    let imgAddTaskFooterMenu = document.getElementById("imgAddTaskFooterMenu");
-    if (addTaskBtnNotClicked) {
-        imgAddTaskFooterMenu.src = "../assets/img/add-task_clicked_btn_footer_mobile.png";
-    } else {
-        imgAddTaskFooterMenu.src = "../assets/img/add_task_default_btn_footer_mobile.png";
-    }
-    addTaskBtnNotClicked = !addTaskBtnNotClicked;  
-}
-
-function showContacts() {
-    let imgContactsFooterMenu = document.getElementById("imgContactsFooterMenu");
-    if (contactsBtnNotClicked) {
-        imgContactsFooterMenu.src = "../assets/img/contacts_clicked_btn_footer_mobile.png";
-    } else {
-        imgContactsFooterMenu.src = "../assets/img/contacts_button_footer_mobile.png";
-    }
-    contactsBtnNotClicked = !contactsBtnNotClicked;
-}
-
-
-function showBoard() {
-    let imgBoardFooterMenu = document.getElementById("imgBoardFooterMenu");
-    if (boardBtnNotClicked) {
-        imgBoardFooterMenu.src = "../assets/img/board_clicked_btn_footer_mobile.png";
-    } else {
-        imgBoardFooterMenu.src = "../assets/img/board_button_footer_mobile.png";
-    }
-    boardBtnNotClicked = !boardBtnNotClicked;
+function setDefaultContentToSummary() {
+    toggleButton('summary');
 }
 // ********************************************************************************
 // ********************************************************************************
