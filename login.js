e1689040ed9830bf7664c2b9140c35459f81d0a9
unknown <j.klee@online.de>
2023-08-29 22:26:37 +0200

    inplement remotestorage functions

--- login.js
+++ login.js
@@ -91,6 +91,20 @@ function enableButton(buttonId){
     button.disabled = false;
 }
 
+/*Remote Storage Implementierung*/
+const STORAGE_TOKEN = 'F4LGRNFMG9GWI4STVSTG89MGMCVVVRZDK3KPVIVF';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
 
 /*Validate Password HTML5 newUser + resetPwd*/
 let newPassword = document.getElementById("newPassword")
