680e8d5af00d45e3f32d149134ee9b8b281e5430
Karwan Hadi Lazgeen <karwanbajdi84@gmail.com>
2023-09-02 11:53:27 +0200

    new array

--- templates/global_arrays.js
+++ templates/global_arrays.js
@@ -1,13 +1,15 @@
 //this array comes from server and has always to be updated
 
-let contactArrayFolder = [
-    {name:'Anton', lastName:'Anfang', mail:'anton@mail.com', phone:'49179111', initials:'', color:'var(--user-orange)'},
-    {name:'Ali', lastName:'Berg', mail:'ali@mail.com', phone:'49176000', initials:'', color:'var(--user-blue)'},
-    {name:'Berta', lastName:'Anfang', mail:'berta@mail.com', phone:'49176000', initials:'', color:'var(--user-yellow)'},
-    {name:'Bert', lastName:'Berg', mail:'bert@mail.com', phone:'49176000', initials:'', color:'var(--user-lila)'},
-    {name:'Cesar', lastName:'Anfang', mail:'cesar@mail.com', phone:'49176000', initials:'', color:'var(--user-red)'},
-    {name:'Dora', lastName:'Berg', mail:'dora@mail.com', phone:'49176000', initials:'', color:'var(--user-rose)'},
-    {name:'Emil', lastName:'Anfang', mail:'emil@mail.com', phone:'49176000', initials:'', color:'var(--user-turquoise)'},
+let contactArray = [
+    {name:'Anton', lastName:'Anfang', mail:'anton@mail.com', phone:'49179111', initials:'AA', color:'var(--user-orange)'},
+    {name:'Ali', lastName:'Berg', mail:'ali@mail.com', phone:'49176000', initials:'AB', color:'var(--user-blue)'},
+    {name:'Berta', lastName:'Anfang', mail:'berta@mail.com', phone:'49176000', initials:'BA', color:'var(--user-yellow)'},
+    {name:'Bert', lastName:'Berg', mail:'bert@mail.com', phone:'49176000', initials:'BB', color:'var(--user-lila)'},
+    {name:'Cesar', lastName:'Anfang', mail:'cesar@mail.com', phone:'49176000', initials:'CA', color:'var(--user-red)'},
+    {name:'Dora', lastName:'Berg', mail:'dora@mail.com', phone:'49176000', initials:'DB', color:'var(--user-rose)'},
+    {name:'Emil', lastName:'Anfang', mail:'emil@mail.com', phone:'49176000', initials:'EA', color:'var(--user-turquoise)'},
+    {name:'Emil', lastName:'Berg', mail:'emil@mail.com', phone:'49176000', initials:'EB', color:'var(--user-green)'},
+    {name:'Fred', lastName:'Anfang', mail:'fred@mail.com', phone:'49176000', initials:'EA', color:'var(--user-orange)'},
 ]
 
 let initialGroups = [];
