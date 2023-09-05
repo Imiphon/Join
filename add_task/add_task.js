da7f43e19adde58e6175aa0f004d97c2f21b8dcc
Karwan Hadi Lazgeen <karwanbajdi84@gmail.com>
2023-09-06 01:42:00 +0200

    defined thr addTask() und developing the checkedUser()

--- add_task/add_task.js
+++ add_task/add_task.js
@@ -1,5 +1,5 @@
 const assignedContacts = [];
-
+const selectedContacts = [];
 
 
 // Function to show or hide options
@@ -38,31 +38,31 @@ function dropDownTemplates() {
     contactArray.forEach((user, index) => {
         dropdown.innerHTML += `
         <div class="options">
-        <span class="profile" id="profile${index}">${showShortName(user.name, user.lastName)}</span>
+        <span class="profile" id="profile${index}">${user.initials}</span>
         <label for="checkboc">${user.name}</label>
-        <input type="checkbox" id="checkbox${index}" value="${user.name}" onclick="checkedUser('${user.name}', '${user.lastName}','${user.color}', ${index})"} onclick>
+        <input type="checkbox" id="checkbox${index}" value="${user.name}" onclick="checkedUser('${user.initials}','${user.color}', ${index})"} onclick>
         </div>
         `
         // Set a random background color for the profile
-        setRandomBackgroundColor(user,index);
+        setBackgroundColor(user, index);
     });
 }
 
 // Array of possible colors for profiles
-const colors = [
-    "red",
-    "blue",
-    "green",
-    "orange",
-    "purple",
-    "pink",
-    "cyan",
-    "magenta",
-    "brown",
-];
+// const colors = [
+//     "red",
+//     "blue",
+//     "green",
+//     "orange",
+//     "purple",
+//     "pink",
+//     "cyan",
+//     "magenta",
+//     "brown",
+// ];
 
 // Function to set a random background color for a profile
-function setRandomBackgroundColor(user,index) {
+function setBackgroundColor(user,index) {
     let userBackground = document.getElementById(`profile${index}`);
     userBackground.style.backgroundColor = user.color;
 }
@@ -73,11 +73,11 @@ function getRandomColor() {
 }
 
 // Function to get both of the letters of the user names;
-function showShortName(userName, userLastName){
-   firstNameFirstLetter = userName.charAt(0);
-   LastNameFirstLetter = userLastName.charAt(0);
-   return firstNameFirstLetter + LastNameFirstLetter;
-}
+// function showShortName(userName, userLastName){
+//    firstNameFirstLetter = userName.charAt(0);
+//    LastNameFirstLetter = userLastName.charAt(0);
+//    return firstNameFirstLetter + LastNameFirstLetter;
+// }
 
 // Initialize flags to track categoryIcon
 let showCategory = false;
@@ -169,7 +169,6 @@ prioBtns.forEach((btn, index)=>{
 
         const boxShadowColor = boxShadowColors[index + 1];
         btn.style.boxShadow = `0px 0px 4px 0px ${boxShadowColor}`;
-        console.log(btn)
         
         // Update the value of priority
     })
@@ -180,18 +179,49 @@ function setPrio(value) {
     prio = value;
 }
 
-// Function to define the assigned user and push the name to the "assignedontacts"-Array to show them in the "selected-user" container
+// Function to define the assigned user and push the name to the "assignedontacts"-Array to show them in the "selected-user" container and push also the selected contacts to the 'selectedContacts'
 
-function checkedUser(userName, userLastName, bColor, index){
-    let chekBoxValue = document.getElementById(`checkbox${index}`).value + " "+userLastName;
-    assignedContacts.push(
-        {
-            'shortName': showShortName(userName, userLastName),
-            'bColor': bColor
+function checkedUser(userInitials, bColor, index){
+    const checkbox = document.getElementById(`checkbox${index}`);
+    const userName = checkbox.value;
+    if(checkbox.checked){
+        assignedContacts.push({'shortName': userInitials, 'bColor': bColor});
+        selectedContacts.push({'shortName': userInitials, 'bColor': bColor, name: userName});
+       
+     }else{
+        const indexToRemove = assignedContacts.findIndex(contact => contact.shortName === userInitials);
+        if(indexToRemove !== -1){
+            assignedContacts.splice(indexToRemove, 1);
+            selectedContacts.splice(indexToRemove, 1);
+        }
+    }
+        showAssignedContactsInContainer()
+            console.log(selectedContacts)
+
+
+}
+
+function selectedContact(userInitials,bColor, index) {
+    const checkbox = document.getElementById(`checkbox${index}`);
+    const userName = checkbox.value;
+
+    if (checkbox.checked) {
+        // Wenn das Kontrollkästchen ausgewählt ist, fügen Sie den Benutzernamen hinzu
+        selectedContacts.push({
+            initials: userInitials,
+            color: bColor,
+            name: userName
         });
+    } else {
+        // Wenn das Kontrollkästchen abgewählt ist, entfernen Sie den Benutzernamen
+        const indexToRemove = selectedContacts.findIndex(contact => contact.name === userName);
+        if (indexToRemove !== -1) {
+            selectedContacts.splice(indexToRemove, 1);
+            showAssignedContactsInContainer();
+        }
+    }
 
-        console.log(chekBoxValue)
-    showAssignedContactsInContainer();
+    console.log(selectedContacts);
 }
 
 
@@ -207,8 +237,8 @@ function showAssignedContactsInContainer(){
             ${profile}
         </span>
         `
-        
     }
+
 }
 
 let editingIndex = -1;
@@ -264,10 +294,52 @@ function editTask(i){
     taskValue.focus();
 }
 
-function addTask(){
-  let todo = addedTasks[0]['toDo'];
-  todo.push({
-    'name': 'Karo'
-  })
-  console.log(todo)
+function addTask(event) {
+    event.preventDefault();
+    // Holen Sie sich die Werte aus dem HTML-Formular
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const date = document.getElementById('date').value;
+    const category = document.getElementById('category').value;
+    const priority = prio; // Der zuvor festgelegte Prioritätswert
+    const subtasks = tasksForSubtasks;
+
+    // Holen Sie sich die ausgewählten Kontakte aus dem assignedContacts Array
+    const assignedContacts = assignedContacts.map(contact => contact.name);
+
+    // Erstellen Sie ein JSON-Objekt mit den gesammelten Informationen
+    const task = {
+        'title': title,
+        'description': description,
+        'date': date,
+        'category': category,
+        'priority': priority,
+        'subTask': subtasks,
+        'assignedContacts': assignedContacts
+    };
+
+    addedTasks[0]['toDo'].push(task);
+
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('category').value = 'Select task Category';
+    prioBtns.forEach(btn => {
+        btn.style.boxShadow = `0px 0px 4px 0px ${boxShadowColors[0]}`;
+    });
+    tasksForSubtasks = [];
+    showAssignedContactsInContainer();
+    renderAddedTask();
+
+    showNotification('Task hinzugefügt');
+
+}
+
+function showNotification(message) {
+    const notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.classList.add('show');
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000); // Die Benachrichtigung verschwindet nach 2 Sekunden
 }
