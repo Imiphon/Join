a72b85866e7d45cd2559761a0b7310ccdaf7a711
unknown <j.klee@online.de>
2023-09-06 20:18:41 +0200

    Update JS functions

--- login.js
+++ login.js
@@ -137,7 +137,6 @@ function openResetPwd(id) {
     resetPwdPage.classList.remove('d-none');
 }
 
-
 function closeResetPwd() {    
     let resetPwdPage = document.getElementById('formResetPwd');
     resetPwdPage.classList.add('d-none');
@@ -215,10 +214,6 @@ async function registerUser(name,email,password) {
     saveUsers();
 }
 
-async function saveUsers() {
-    await setItem('users', JSON.stringify(users));
-}
-
 function getInput(id) {
     return document.getElementById(id).value;
 }
@@ -249,10 +244,6 @@ function enableButton(buttonId) {
     button.disabled = false;
 }
 
-/*Remote Storage Implementierung*/
-const STORAGE_TOKEN = 'F4LGRNFMG9GWI4STVSTG89MGMCVVVRZDK3KPVIVF';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-
 async function loadUsers() {
     try {
         users = JSON.parse(await getItem('users'));
@@ -261,20 +252,10 @@ async function loadUsers() {
     }
 }
 
-async function setItem(key, value) {
-    const payload = {key, value, token: STORAGE_TOKEN};
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
+async function saveUsers() {
+    await setItem('users', JSON.stringify(users));
 }
 
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        if (res.data) { 
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
-    });
-}
 
 /*Validate Password Match HTML5 newUser + resetPwd*/
 let newPassword = document.getElementById("newPassword")
