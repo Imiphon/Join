94bcc86e6520e6785a746901eb13f5e88181d6fe
Your Name <you@example.com>
2023-09-08 09:41:27 -0400

    $*

--- up/up.sh
+++ up/up.sh
@@ -1,9 +1,9 @@
 git pull
 git add .
-git commit -m '${*}'
+git commit -m '$*'
 git push
 
 
 # der Ordner up.sh liegt im Ordner up
 # vor dem ersten Mal: chmod +x up/up.sh
-# aufruf mit: up/up.sh "t"
+# aufruf mit: up/up.sh "text"
