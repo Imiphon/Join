09d616a8bdd16ffcbe3baba4ebf2fea63906361d
Your Name <you@example.com>
2023-09-10 13:38:30 -0400

    change useerColors Array to global_arrays.js

--- templates/global_arrays.js
+++ templates/global_arrays.js
@@ -215,4 +215,18 @@ async function getItem(key) {
     });
 }
  
+//=============================================
+// COLORS
+//=============================================
 
+let userColors = {
+  grey: "var(--user-grey)",
+  orange: "var(--user-orange)",
+  lila: "var(--user-lila)",
+  blue: "var(--user-blue)",
+  rose: "var(--user-rose)",
+  yellow: "var(--user-yellow)",
+  green: "var(--user-green)",
+  red: "var(--user-red)",
+  turquoise: "var(--user-turquoise)",
+};
